359c52992ff5c296e81ca4f5b0634579633d02e6
Joel Pitzer <jpitzer5@gmail.com>
2025-01-17 22:03:21 -0800

    added a header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import icon from "../assets/chef-claude-icon.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={icon} alt="robot chef icon" />
+            <h1>
+                Chef Claude
+            </h1>
+        </header>
+    )
+}
