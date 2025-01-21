d88f9bc6f9cef6ebb8fc93896dbd602c02fa1792
Joel Pitzer <jpitzer5@gmail.com>
2025-01-20 21:34:51 -0800

    added functionality to scroll to recipe once it is retreived and displayed

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -2,7 +2,7 @@ import ReactMarkdown from "react-markdown"
 
 export default function ClaudeRecipe(props) {
     return (
-        <section className="suggested-recipe-container" aria-live="polite">
+        <section ref={props.ref} className="suggested-recipe-container" aria-live="polite">
             <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import ClaudeRecipe from "./ClaudeRecipe"
 import IngredientsList from "./IngredientsList"
 import { getRecipeFromMistral } from "../ai"
@@ -19,6 +19,14 @@ export default function Main() {
         setRecipe(recipeMarkdown)
     }
 
+    const recipeSection = useRef(null)
+
+    useEffect(() => {
+        if (recipe && recipeSection.current) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipe])
+
     return (
         <main>
             <form action={handleSubmit} className="add-ingredient-form">
@@ -31,7 +39,7 @@ export default function Main() {
                 <button>Add ingredient</button>
             </form>
             {ingredients.length ? <IngredientsList ingredients={ingredients} getRecipe={getRecipe} /> : null}
-            {recipe && <ClaudeRecipe recipe={recipe} />}
+            {recipe && <ClaudeRecipe recipe={recipe} ref={recipeSection} />}
         </main>
     )
 }
